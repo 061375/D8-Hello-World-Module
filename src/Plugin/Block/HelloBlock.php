39e00a75441f16d79ccb5613c60f355166cf979a
Jeremy Heminger <jeremy@geomail.info>
2017-01-11 14:35:11 -0800

    process the block

--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -40,4 +40,10 @@ class HelloBlock extends BlockBase  implements BlockPluginInterface {
 
     return $form;
   }
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('name', $form_state->getValue('hello_block_name'));
+  }
 }
